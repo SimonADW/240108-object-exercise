1142c9462321aec9f5915e0a787018796c5dd2a0
SimonADW <simon-winter@outlook.com>
2024-01-08 10:02:35 +0100

    Inital commit - Setup filesstructure and constructior layout

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+const function = CreateStopwatch() = {
+	return {
+		this.duration: 0, 
+		this.start: function() {
+
+		}
+		this.stop: function() {
+
+		}
+		this.reset: function() {
+			this.duration = 0
+		}
+	}
+
+}
+
+sw = new CreateStopwatch()
+console.log(sw.du);
