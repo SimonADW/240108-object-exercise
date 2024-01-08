77dcbe8b0dbe0fae82cd9796f56c4f24771c0b9a
SimonADW <simon-winter@outlook.com>
2024-01-08 10:33:07 +0100

    Declare functions, and Date objects

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 
 
-const function = CreateStopwatch() = {
-	return {
-		this.duration: 0, 
-		this.start: function() {
+function CreateStopwatch() {
+	let startTime;
+	this.duration = 0, 
 
-		}
-		this.stop: function() {
-
-		}
-		this.reset: function() {
-			this.duration = 0
-		}
+	this.start = function() {
+		startTime = new Date().getSeconds();							
 	}
-
+	this.stop = function() {
+		this.duration = new Date().getSeconds() - startTime;
+		console.log(sw.duration);		
+	}
+	this.reset = function() {
+		this.duration = 0
+	}
 }
 
 sw = new CreateStopwatch()
-console.log(sw.du);
+
